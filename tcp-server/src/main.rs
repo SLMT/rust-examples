c95ddba4d71795e22c47c4140dcf900a2440c412
SLMT <sam123456777@gmail.com>
2016-03-17 20:50:16 +0800

    Add echo function to tcp-server

--- tcp-server/src/main.rs
+++ tcp-server/src/main.rs
@@ -1,5 +1,7 @@
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use std::io::prelude::{Write};
+use std::io::{BufReader, BufRead, BufWriter};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:54321").unwrap();
@@ -21,13 +23,27 @@ fn main() {
 }
 
 fn handle_client(stream: TcpStream) {
-    match stream.peer_addr() {
-        Ok(addr) => {
-            println!("Get a connection from {}", addr);
-        }
-        Err(e) => {
-            println!("Error Message: {}", e);
-        }
-    }
+    // Check IP Address
+    let addr = stream.peer_addr().unwrap();
+    println!("Get a connection from {}", addr);
 
+    // Create a buffer reader and a writer
+    // XXX: Why can we just pass immutable reference
+    // istead of mutable reference here ?
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
+
+    // Send a message
+    writer.write(b"Hello Meow!! Please enter a message:\n").unwrap();
+    writer.flush().unwrap();
+
+    // Receive a message
+    let mut message = String::new();
+    reader.read_line(&mut message).unwrap();
+
+    // Echo it
+    writer.write(b"Your message: ").unwrap();
+    writer.write(message.trim().as_bytes()).unwrap();
+
+    writer.write(b"\nThank you for your using.\n").unwrap();
 }
