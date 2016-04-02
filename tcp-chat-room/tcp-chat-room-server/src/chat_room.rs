827c7d0c8088b4d685d13c566007627d8fecee58
yslin <yslin@netdb.cs.nthu.edu.tw>
2016-04-02 23:33:34 +0800

    Add a chat room
    - A chat room contains a name list indicating who are in the room.
    - This server **might** be able to print the names of users in the room
      now. (not tested)

--- tcp-chat-room/tcp-chat-room-server/src/chat_room.rs
+++ tcp-chat-room/tcp-chat-room-server/src/chat_room.rs
@@ -1,8 +1,22 @@
 
+// TODO: Remove this attribute after finishing this project
+#[derive(Debug)]
 pub struct ChatRoom {
-    nameList : Vec<String>
+    name_list : Vec<String>
 }
 
 impl ChatRoom {
-    pub 
+    pub fn new() -> ChatRoom {
+        ChatRoom {
+            name_list: Vec::new()
+        }
+    }
+
+    pub fn join(&mut self, name: String) {
+        self.name_list.push(name);
+    }
+
+    pub fn get_name_list(&self) -> Vec<String> {
+        self.name_list.clone()
+    }
 }
--- tcp-chat-room/tcp-chat-room-server/src/main.rs
+++ tcp-chat-room/tcp-chat-room-server/src/main.rs
@@ -2,15 +2,31 @@ use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::io::prelude::{Write};
 use std::io::{BufReader, BufRead, BufWriter};
+use std::sync::{Arc, RwLock};
+
+// We must declare module `chat_room` before `use`ing it
+mod chat_room;
+
+// Don't forget to declare the module
+use chat_room::ChatRoom;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:54321").unwrap();
+    let room_lock = Arc::new(RwLock::new(ChatRoom::new()));
+
+    {
+        let mut room = room_lock.write().unwrap();
+        room.join(String::from("Haha"));
+        room.join(String::from("Kerker"));
+        room.join(String::from("Papa"));
+    }
 
     for stream in listener.incoming() {
         match stream {
             Ok(to_client_stream) => {
+                let rl = room_lock.clone();
                 thread::spawn(move || {
-                    handle_client(to_client_stream);
+                    handle_client(to_client_stream, rl);
                 });
             }
             Err(e) => {
@@ -22,7 +38,7 @@ fn main() {
     drop(listener);
 }
 
-fn handle_client(stream: TcpStream, chatRoom: ChatRoom) {
+fn handle_client(stream: TcpStream, room_lock: Arc<RwLock<ChatRoom>>) {
     // Check IP Address
     let addr = stream.peer_addr().unwrap();
     println!("Get a connection from {}", addr);
@@ -31,17 +47,40 @@ fn handle_client(stream: TcpStream, chatRoom: ChatRoom) {
     let mut reader = BufReader::new(&stream);
     let mut writer = BufWriter::new(&stream);
 
-    // Send a message
-    writer.write(b"Hello Meow!! Please enter a message:\n").unwrap();
-    writer.flush().unwrap();
+    print_online_list(&mut writer, &room_lock);
 
-    // Receive a message
-    let mut message = String::new();
-    reader.read_line(&mut message).unwrap();
+    //
+    // // Send a message
+    // writer.write(b"Hello Meow!! Please enter a message:\n").unwrap();
+    // writer.flush().unwrap();
+    //
+    // // Receive a message
+    // let mut message = String::new();
+    // reader.read_line(&mut message).unwrap();
+    //
+    // // Echo it
+    // writer.write(b"Your message: ").unwrap();
+    // writer.write(message.trim().as_bytes()).unwrap();
+    //
+    // writer.write(b"\nThank you for your using.\n").unwrap();
+    // {
+    //     room_lock.write().unwrap().join(String::from("Haha"));
+    // }
+}
 
-    // Echo it
-    writer.write(b"Your message: ").unwrap();
-    writer.write(message.trim().as_bytes()).unwrap();
+fn print_online_list(writer: &mut BufWriter<&TcpStream>, room_lock: &Arc<RwLock<ChatRoom>>) {
+    let list;
 
-    writer.write(b"\nThank you for your using.\n").unwrap();
+    // Acquire the read lock of the chat room.
+    // Then, get the name list from it.
+    {
+        list = room_lock.read().unwrap().get_name_list();
+    } // Release the read lock here
+
+    // Print the online list
+    writer.write(b"=== Online List ===\n").unwrap();
+    for name in &list {
+        writer.write_fmt(format_args!("{}\n", name)).unwrap();
+    }
+    writer.write(b"===================\n").unwrap();
 }
